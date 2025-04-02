5c56a71d0ee262db352ad3d44d6c263e866a703d
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-04-02 09:42:04 -0300

    ex02

---
+++ ex02/ex02/ex02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApp {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("Type the radius value: ");
+            double radius = double.Parse(Console.ReadLine());
+            double pi = 3.14159;
+            double field = pi * (radius * radius);
+
+            Console.Write($"Output: {field:F4}");
+        }
+    }
+}
