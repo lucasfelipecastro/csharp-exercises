8d7ff6c054ae3ab41edec21027f74d4830a7b3f3
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-04-08 06:16:25 -0300

    ex07

---
+++ ex07/ex07/ex07/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("Type a int number: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            if (n1 < 0) {
+                Console.WriteLine($"The number {n1} is NEGATIVE");
+            }
+            else {
+                Console.WriteLine($"The number {n1} is POSITIVE");
+
+            }
+        }
+    }
+}
