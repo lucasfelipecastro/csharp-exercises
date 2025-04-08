2a61f1de10d4a7da7090896aeed14eab86d1ecc3
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-04-08 06:16:46 -0300

    ex08

---
+++ ex08/ex08/ex08/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("Type a int number: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            if (n1 % 2 != 0) {
+                Console.WriteLine($"The number {n1} is ODD");
+            }
+            else {
+                Console.WriteLine($"The number {n1} is EVEN");
+
+            }
+        }
+    }
+}
