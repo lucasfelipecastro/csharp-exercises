6416580425aa124a59ee55f942176760a51a92ca
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-04-08 06:18:39 -0300

    ex13

---
+++ ex13/ex13/ex13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Program {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Enter a number: ");
+            int x = int.Parse(Console.ReadLine());
+      
+
+            for (int i = 1; i <= x; i++) {
+                if (i % 2 != 0) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
