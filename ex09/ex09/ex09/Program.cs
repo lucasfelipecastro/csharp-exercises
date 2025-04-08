f0a94ebe20f16f79091d64be0e39c101594f3fbb
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-04-08 06:17:45 -0300

    ex10

---
+++ ex09/ex09/ex09/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Type 2 integer numbers: ");
+            Console.WriteLine("------------------------");
+
+            Console.Write("A: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("B: ");
+            int b = int.Parse(Console.ReadLine());
+
+            if (a == b) {
+                Console.WriteLine("They are multiples");
+            }
+            else if (a > b) {
+                if (a % b == 0) {
+                    Console.WriteLine("They are multiples");
+                }
+                else {
+                    Console.WriteLine("They are NOT multiples");
+                }
+            }
+            else {
+                if (b % a == 0) {
+                    Console.WriteLine("They are multiples");
+                }
+                else {
+                    Console.WriteLine("They are NOT multiples");
+                }
+            }
+        }
+    }
+}
---
+++ ex10/ex10/ex10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Program {
+    class Program {
+        static void Main(string[] args) {
+            int password = 2002;
+
+            Console.Write("Type the password: ");
+            int user_password = int.Parse(Console.ReadLine());
+
+            while (user_password != password) {
+                Console.WriteLine("Password Invalid!");
+                Console.Write("Type the password: ");
+                user_password = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Password Valid!");
+        }
+    }
+}
