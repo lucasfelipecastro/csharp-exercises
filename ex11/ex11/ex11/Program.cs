f666f52656a1aaf0b012695d5521d97557b459d0
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-04-08 06:18:00 -0300

    ex11

---
+++ ex11/ex11/ex11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Program {
+    class Program {
+        static void Main(string[] args) {
+            while (true) {
+                Console.Write("Coordinate X: ");
+                int x = int.Parse(Console.ReadLine());
+                Console.Write("Coordinate Y: ");
+                int y = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                if (x == 0 || y == 0) {
+                    break;
+                }
+
+                if (x > 0 && y > 0) {
+                    Console.WriteLine("Quadrant I");
+                }
+                else if (x < 0 && y > 0) {
+                    Console.WriteLine("Quadrant II");
+                }
+                else if (x < 0 && y < 0) {
+                    Console.WriteLine("Quadrant III");
+                }
+                else if (x > 0 && y < 0) {
+                    Console.WriteLine("Quadrant IV");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
