aeddb0cbdc258305bf2a6fe3ac723dc44ff45ada
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-04-28 09:43:56 -0300

    exercises-class02

---
+++ ex-class/ex02/ex02/ex02/Employee.cs
@@ -0,0 +1,6 @@
+﻿namespace ex02 {
+    internal class Employee {
+        public string name;
+        public double salary;
+    }
+}
---
+++ ex-class/ex02/ex02/ex02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ex02;
+
+namespace Ex02 {
+    class Program {
+        static void Main(string[] args) {
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+
+            Console.WriteLine("First employee: ");
+            Console.WriteLine("---------------");
+
+            Console.Write("Name: ");
+            employee1.name = Console.ReadLine();
+            Console.Write("Salary: ");
+            employee1.salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Second employee: ");
+            Console.WriteLine("---------------");
+
+
+            Console.Write("Name: ");
+            employee2.name = Console.ReadLine();
+            Console.Write("Salary: ");
+            employee2.salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("---------------");
+            double average_salary = (employee1.salary + employee2.salary) / 2;
+            Console.WriteLine($"Average Salary: {average_salary:F2}");
+        }
+    }
+}
