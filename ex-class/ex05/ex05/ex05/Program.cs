0de88a8c08b164e87b764e723f611c4f8241abb5
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-04-28 10:15:51 -0300

    exercises-class05

---
+++ ex-class/ex05/ex05/ex05/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Program {
+    public class Program {
+        public static void Main(string[] args) {
+            Student student = new Student();
+
+            Console.Write("Student Name: ");
+            student.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter the student's three grades: ");
+            student.Grade1 = double.Parse(Console.ReadLine());
+            student.Grade2 = double.Parse(Console.ReadLine());
+            student.Grade3 = double.Parse(Console.ReadLine());
+
+            double final_grade = student.Grade1 + student.Grade2 + student.Grade3;
+            Console.WriteLine($"Final Grade: {final_grade}");
+
+            if (final_grade >= 60) {
+                Console.WriteLine("Student passed.");
+            }
+            else {
+                Console.WriteLine("Student failed.");
+                double missing = 60 - final_grade;
+                Console.WriteLine($"Missing: {missing}");
+            }
+        }
+    }
+}
---
+++ ex-class/ex05/ex05/ex05/Student.cs
@@ -0,0 +1,8 @@
+﻿public class Student {
+    public string Name;
+    public double Grade1;
+    public double Grade2;
+    public double Grade3;
+    public double Average;
+}
+
