b2ba52df72d7180f3ec5723d9d8a395f854d8809
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-04-28 09:45:38 -0300

    exercises-class04

---
+++ ex-class/ex04/ex04/ex04/Employee.cs
@@ -0,0 +1,14 @@
+﻿public class Employee {
+    public string name;
+    public double gross_salary;
+    public double tax;
+
+    public double net_salary() {
+        return gross_salary - tax;
+    }
+
+    public void increase_salary(double percentage) {
+        gross_salary += (gross_salary * percentage / 100);
+
+    }
+}
---
+++ ex-class/ex04/ex04/ex04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Program {
+    public class Program {
+        static void Main(string[] args) {
+            Employee employee = new Employee();
+
+            Console.Write("Name: ");
+            employee.name = Console.ReadLine();
+
+            Console.Write("Gross Salary: ");
+            employee.gross_salary = double.Parse(Console.ReadLine());
+
+            Console.Write("Tax: ");
+            employee.tax = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Employee INFOS: {employee.name}, ${employee.gross_salary} ");
+
+            Console.Write("Enter the percentage to increase the salary (EXAMPLE: 10.0): ");
+            double percentage = double.Parse(Console.ReadLine());
+
+            employee.increase_salary(percentage);
+            Console.WriteLine($"Updated INFOS: {employee.name}, ${employee.net_salary()}"); // Use net_salary() to display updated salary
+        }
+    }
+}
