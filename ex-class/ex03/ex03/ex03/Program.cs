184b4249d4e84f54fd9751fcf5175c7e03b26702
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-04-28 09:44:13 -0300

    exercises-class03

---
+++ ex-class/ex03/ex03/ex03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ex03;
+
+namespace Ex03 {
+    class Program {
+        static void Main(string[] args) {
+            Rectangle rectangle = new Rectangle();
+            Console.WriteLine("Enter the width and heigth of the Rectangle: ");
+
+            Console.Write("Width: ");
+            rectangle.Width = double.Parse(Console.ReadLine());
+
+            Console.Write("Height: ");
+            rectangle.Height = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"Area: {rectangle.Area():F2}");
+            Console.WriteLine($"Perimeter: {rectangle.Perimeter():F2}");
+            Console.WriteLine($"Diagonal: {rectangle.Diagonal():F2}");
+        }
+    }
+}
---
+++ ex-class/ex03/ex03/ex03/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿namespace ex03 {
+    class Rectangle {
+        public double Width;
+        public double Height;
+
+        public double Area() {
+            return Width * Height;
+        }
+
+        public double Perimeter() {
+            return 2 * (Width + Height);
+        }
+
+        public double Diagonal() {
+            return Math.Sqrt(Width * Width + Height * Height);
+        }
+    }
+}
