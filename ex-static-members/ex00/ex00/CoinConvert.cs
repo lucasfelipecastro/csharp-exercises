892a006bebc448abc3e72989f07620f25060ed81
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-05-08 09:01:44 -0300

    ex15

---
+++ ex-static-members/ex00/ex00/CoinConvert.cs
@@ -0,0 +1,10 @@
+﻿namespace ex00 {
+    class CoinConvert {
+        public static double Iof = 6.0;
+
+        public static double ConvertDollarToBrl(double quote, double amount) {
+            double total = quote * amount;
+            return total * (1 + Iof / 100.0);
+        }
+    }
+}
---
+++ ex-static-members/ex00/ex00/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ex00;
+
+namespace Program {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Dollar Quote: $");
+            double quote = double.Parse(Console.ReadLine());
+
+            Console.Write("How much dollars are you buying: $");
+            double amount = double.Parse(Console.ReadLine());
+
+            double result = CoinConvert.ConvertDollarToBrl(quote, amount);
+
+            Console.WriteLine($"Amount to be paid in BRL: R${result:F2}");
+        }
+    }
+
+}
